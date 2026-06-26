f1344879c99f627fa44590c12691be24700d1244
Ayalk15 <103822329+Ayalk15@users.noreply.github.com>
2026-06-26 12:59:32 +0300

    Create teams.data.js

---
+++ teams.data.js
@@ -0,0 +1,6 @@
+export const TEAMS = {
+  MTA: { name: "מכבי תל אביב", badge: "🟡🔵" },
+  MHA: { name: "מכבי חיפה", badge: "🟢" },
+  HBS: { name: "הפועל באר שבע", badge: "🔴" },
+  // ... להוסיף את שאר הקבוצות
+};
