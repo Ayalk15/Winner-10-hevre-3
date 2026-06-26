672ae60f32b2194a0eea83651905b498f083844f
Ayalk15 <103822329+Ayalk15@users.noreply.github.com>
2026-06-26 13:00:53 +0300

    Delete teams.data.js

--- teams.data.js
+++
@@ -1,6 +0,0 @@
-export const TEAMS = {
-  MTA: { name: "מכבי תל אביב", badge: "🟡🔵" },
-  MHA: { name: "מכבי חיפה", badge: "🟢" },
-  HBS: { name: "הפועל באר שבע", badge: "🔴" },
-  // ... להוסיף את שאר הקבוצות
-};
